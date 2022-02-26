7c98af95e66b1d5b102f5a474704d828bbec6347
thoma <rusieckithomas@gmail.com>
2022-02-26 05:51:27 -0300

    Initial commit

---
+++ app/src/main/java/com/buggysoft/sendit/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.buggysoft.sendit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
