6a7507c458de82825bdaa5bd67227421dad467d2
thoma <rusieckithomas@gmail.com>
2022-02-28 06:40:54 -0300

    Nav controller and toolbar added

--- app/src/main/java/com/buggysoft/sendit/MainActivity.kt
+++ app/src/main/java/com/buggysoft/sendit/MainActivity.kt
@@ -1,33 +1,92 @@
 package com.buggysoft.sendit
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private var user: FirebaseUser? = null
 
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { res ->
         this.onSignInResult(res)
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Handle the splash screen transition.
-        val splashScreen = installSplashScreen()
-        auth = Firebase.auth
 
-        createSignInIntent()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Handle the splash screen
+        auth = Firebase.auth
+        user = auth.currentUser
+        val splashScreen = installSplashScreen()
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+        if(user == null){
+            Log.d(ContentValues.TAG, "User is not signed in")
+            createSignInIntent()
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_About -> {
+            // User chose About
+            //TODO later maybe show a toast meanwhile?
+            true
+        }
+
+        R.id.action_Pair -> {
+            //TODO go to pair fragment
+            true
+        }
+        R.id.action_logout -> {
+
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    FirebaseAuth.getInstance().signOut()
+                    createSignInIntent()
+                }
+            true
+        }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
     }
 
 
@@ -35,9 +94,10 @@ class MainActivity : AppCompatActivity() {
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
             // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
+            user = FirebaseAuth.getInstance().currentUser
             // ...
         } else {
+
             // Sign in failed. If response is null the user canceled the
             // sign-in flow using the back button. Otherwise check
             // response.getError().getErrorCode() and handle the error.
